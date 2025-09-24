0367e2d54fd904f45e6744d6ca625624b0d83288
Ajay Sahani <sajay3159@gmail.com>
2025-09-24 20:41:04 +0530

    remove unused

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import Card from "../UI/Card/Card";
 import classes from "./Home.module.css";
 
-const Home = (props) => {
+const Home = () => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
